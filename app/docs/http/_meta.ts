26f4c77e8cbdc2c6e3b4fc7c4048d072317e0779
Kent Ng <kent.ng201@gmail.com>
2026-04-15 11:52:16 +0800

    add context page

--- app/docs/http/_meta.ts
+++ app/docs/http/_meta.ts
@@ -1,6 +1,7 @@
 export default {
     microservice: { title: 'Microservices' },
     routing: { title: 'Routing' },
+    context: { title: 'Context' },
     middleware: { title: 'Middleware' },
     controllers: { title: 'Controllers' },
     openapi: { title: 'API Testing' }
